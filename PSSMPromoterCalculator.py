0fe48819d6228ff04109be456b36b848d07ca714
ellinium <ellina.trofimova@gmail.com>
2023-04-18 20:38:00 +1000

    pass a file name to process

--- PSSMPromoterCalculator.py
+++ PSSMPromoterCalculator.py
@@ -1,34 +1,58 @@
 import random
 import pandas as pd
 from datetime import datetime
-from Bio.Seq import Seq
+from Bio import Seq, SeqIO
+import sys, os
+#For colab
+#from google.colab import files
+##sys.path.append(os.path.abspath('PSSM_PromoterTool/'))
 import pssm_util
 
 OUTPUT_FILE_NAME = "PSSMPromoterCalculator"
 
+def get_gene_sequence():
+    gene_sequence = ""
+    if len(sys.argv) < 2:
+        raise Exception('Please provide a file name with a gene sequence.')
+
+    gene_file_name = sys.argv[1]
+
+    f = open(gene_file_name, "r")
+    f_content = f.read()
+
+    if '>' in f_content:
+        records = list(SeqIO.parse(gene_file_name, "fasta"))
+        gene_sequence = str(records[0].seq)
+    else:
+        gene_sequence = f_content
+    return str.upper(gene_sequence)
+
+
 if __name__ == "__main__":
 
     from dask.distributed import Client, LocalCluster
-    cluster = LocalCluster()  # Launches a scheduler and workers locally
-    client = Client(cluster)
-
+    # cluster = LocalCluster()  # Launches a scheduler and workers locally
+    # client = Client(cluster)
+    client = Client()
+    # import dask
+    # dask.config.set(scheduler='processes')
     start_time = datetime.now()
-    salis_calc_run_num = 0
 
     #sequence = "".join([random.choice(['A','G','C','T']) for x in range(1000)])
-    sequence = "".join([random.choice(['A','G','C','T']) for x in range(100)])
+    #sequence = "".join([random.choice(['A','G','C','T']) for x in range(100)])
 
-    sequence = "TCTATGCTCCAGGGCGATTAGGGAACAGCGTGTTGCTGGTCAGTAGTGTACCCTAGCCCACATAGCTACTTTTACTTCGTCCGTTCAGCGGACAAACGCT"
+    #sequence = "TCTATGCTCCAGGGCGATTAGGGAACAGCGTGTTGCTGGTCAGTAGTGTACCCTAGCCCACATAGCTACTTTTACTTCGTCCGTTCAGCGGACAAACGCT"
     #sequence = "ATGGTACGCTGGACTTTGTGGGATACCCTCGCTTTCCTGCTCCTGTTGAGTTTATTGCTGCCGTCATTGCTTATTATGTTCATCCCGTCAACATTCAAACGGCCTGTCTCATCATGGAAGGCGCTGAATTTACGGAAAACATTATTAATGGCGTCGAGCGTCCGGTTAAAGCCGCTGAATTGTTCGCGTTTACCTTGCGTGTACGCGCAGGAAACACTGACGTTCTTACTGACGCAGAAGAAAACGTGCGTCAAAAATTACGTGCGGAAAGAATGA"
     #sequence = "ATGAGTCAAGTTACTGAACAATCCGTACGTTTCCAGACCGCTTTGGCCTCTATTAAGCTCATTCAGGCTTCTGCCGTTTTGGATTTAACCGAAGATGATTTCGATTTTCTGACGAGTAACAAAGTTTGGATTGCTACTGACCGCTCTCGTGCTCGTCGCTGCGTTGAAGCTTGCGTTTACGGTACGCTGGACTTTGTGGGATACCCTCGCTTTCCTGCTCCTGTTGAGTTTATTGCTGCCGTCATTGCTTATTATGTTCATCCCGTCAACATTCAAACGGCCTGTCTCATCATGGAAGGCGCTGAATTTACGGAAAACATTATTAATGGCGTCGAGCGTCCGGTTAAAGCCGCTGAATTGTTCGCGTTTACCTTGCGTGTACGCGCAGGAAACACTGACGTTCTTACTGACGCAGAAGAAAACGTGCGTCAAAAATTACGTGCGGAGGGTGTGATGTAA"
     #sequence = "atgtctaaaggtaaaaaacgttctggcgctcgccctggtcgtccgcagccgttgcgaggtactaaaggcaagcgtaaaggcgctcgtctttggtatgtaggtggtcaacaattttaa"
 
-    sequence = str.upper(sequence)
+
+    sequence = get_gene_sequence()
     calc = pssm_util.Promoter_Calculator()
     calc.run(sequence, TSS_range = [0, len(sequence)])
     fwd_TSS_df, rev_TSS_df = calc.output()
     end_time = datetime.now()
-    print('Duration of 1 salis calc run: {}'.format(end_time - start_time))
+    ##print('Duration of 1 salis calc run: {}'.format(end_time - start_time))
 
     #fwd_TSS_df = TSS_results_to_df(output['Forward_Predictions_per_TSS'])
     #rev_TSS_df = TSS_results_to_df(output['Reverse_Predictions_per_TSS'])
@@ -104,12 +128,12 @@ if __name__ == "__main__":
     res_final_df_min = pd.concat([min_fwd_TSS_df, min_rev_TSS_df], ignore_index=True, sort=False)
     #res_final_df = res_final_df.drop_duplicates()
     res_final_df_max = res_final_df_max.drop_duplicates(
-        subset=['hex35', 'hex10', 'Tx_rate', 'ITR', 'original_TSS', 'direction'],
+        subset=['hex35', 'hex10', 'Tx_rate', 'ITR', 'original_record', 'direction'],
         keep='last').reset_index(drop=True)
 
 
     res_final_df_min = res_final_df_min.drop_duplicates(
-        subset=['hex35', 'hex10', 'Tx_rate', 'ITR', 'original_TSS', 'direction'],
+        subset=['hex35', 'hex10', 'Tx_rate', 'ITR', 'original_record', 'direction'],
         keep='last').reset_index(drop=True)
 
     res_final_df_max['AA_hex35'] = res_final_df_max['hex35'].apply(lambda x: str(Seq(x).translate()))
@@ -148,58 +172,60 @@ if __name__ == "__main__":
     new_min_rev_Tx_rate_df = res_final_df_min_rev_df.sort_values(by='Tx_rate', ascending=False)
     new_min_rev_Tx_rate = new_min_rev_Tx_rate_df['Tx_rate'].tail(1).values[0]
 
-    column_list = ["new_sequence", "promoter_sequence", "TSS", "Tx_rate", "UP", "hex35", "PSSM_hex35", "AA_hex35", "spacer", "hex10", "PSSM_hex10", "AA_hex10", "disc", "ITR", "dG_total", "dG_10", "dG_35", "dG_disc", "dG_ITR", "dG_ext10", "dG_spacer", "dG_UP", "dG_bind",  "UP_position", "hex35_position", "spacer_position", "hex10_position", "disc_position"]
+    column_list = ["new_gene_sequence", "promoter_sequence", "TSS", "Tx_rate", "UP", "hex35", "PSSM_hex35", "AA_hex35", "spacer", "hex10", "PSSM_hex10", "AA_hex10", "disc", "ITR", "dG_total", "dG_10", "dG_35", "dG_disc", "dG_ITR", "dG_ext10", "dG_spacer", "dG_UP", "dG_bind",  "UP_position", "hex35_position", "spacer_position", "hex10_position", "disc_position"]
 
+    print("\n")
     print("The minimum transcription rate for the sequence (forward) " + str(def_fwd_min_tx_rate))
     if len(res_final_df_min_fwd_df) > 1:
+        min_fwd_output_file = OUTPUT_FILE_NAME + "_MIN_FWD_results.csv"
         if float(new_min_fwd_Tx_rate) < float(def_fwd_min_tx_rate):
             print ("can be decreased up to " + str(new_min_fwd_Tx_rate))
-            print("using the following promoters:")
-            res_final_df_max_fwd_df.to_csv(OUTPUT_FILE_NAME + "_MIN_FWD_results.csv", columns = column_list)
+            print("using the promoters in the " + min_fwd_output_file)
+            res_final_df_max_fwd_df.to_csv(min_fwd_output_file, columns = column_list)
+
+            ##files.download(min_fwd_output_file)
+
     else:
         print(" cannot be further decreased")
 
+    print("\n")
     print("The minimum transcription rate for the sequence (reverse) " + str(def_rev_min_tx_rate))
     if len(res_final_df_min_rev_df) > 1:
+        min_rev_output_file = OUTPUT_FILE_NAME + "_MIN_REV_results.csv"
         if float(new_min_rev_Tx_rate) < float(def_rev_min_tx_rate):
             print ("can be decreased up to " + str(new_min_rev_Tx_rate))
-            print("using the following promoters:")
-            res_final_df_max_fwd_df.to_csv(OUTPUT_FILE_NAME + "_MIN_REV_results.csv", columns = column_list)
+            print("using the promoters in the " + min_rev_output_file)
+            res_final_df_max_fwd_df.to_csv(min_rev_output_file, columns = column_list)
+
+            ##files.download(min_rev_output_file)
 
     else:
         print(" cannot be further decreased")
+
+    print("\n")
     print("The maximum transcription rate for the sequence (forward) " + str(def_fwd_max_tx_rate))
     if len(res_final_df_max_fwd_df) > 1:
+        max_fwd_output_file = OUTPUT_FILE_NAME + "_MAX_FWD_results.csv"
         if float(new_max_fwd_Tx_rate) > float(def_fwd_max_tx_rate):
             print ("can be increased up to " + str(new_max_fwd_Tx_rate))
-            print("using the following promoters:")
-            res_final_df_max_fwd_df.to_csv(OUTPUT_FILE_NAME + "_MAX_FWD_results.csv", columns = column_list)
+            print("using the promoters in the " + max_fwd_output_file)
+            res_final_df_max_fwd_df.to_csv(max_fwd_output_file, columns = column_list)
+            ##files.download(max_fwd_output_file)
 
     else:
         print(" cannot be further increased")
-
+    print("\n")
     print("The maximum transcription rate for the sequence (reverse) " + str(def_rev_max_tx_rate))
     if len(res_final_df_max_rev_df) > 1:
+        max_rev_output_file = OUTPUT_FILE_NAME + "_MAX_REV_results.csv"
         if float(new_max_rev_Tx_rate) > float(def_rev_max_tx_rate):
             print ("can be increased up to " + str(new_max_rev_Tx_rate))
-            print("using the following promoters:")
-            res_final_df_max_fwd_df.to_csv(OUTPUT_FILE_NAME + "_MAX_REV_results.csv", columns = column_list)
+            print("using the promoters in the " + max_rev_output_file)
+            res_final_df_max_fwd_df.to_csv(max_rev_output_file, columns = column_list)
+            ##files.download(max_rev_output_file)
 
     else:
         print(" cannot be further increased")
-
-
-
-
-    print("Mac Tx rate FWD = " + str(def_fwd_max_tx_rate))
-    print("Mac Tx rate REV = " + str(def_rev_max_tx_rate))
-    print("Min Tx rate FWD = " + str(def_fwd_min_tx_rate))
-    print("Min Tx rate REV = " + str(def_rev_min_tx_rate))
-
-
 
     end_time = datetime.now()
     print('Duration: {}'.format(end_time - start_time))
-
-# +BOTTOM sequences
-#Display synonymous only if Tx_rate more than it was
