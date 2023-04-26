04a4e66e0e925466a36ec34b92269ed5923234cd
ellinium <ellina.trofimova@gmail.com>
2023-04-26 13:15:06 +1000

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='PSSM-promoter-tool',
+setup(name='pssm-promoter-tool',
       version='1.0.0',
       description='The tools applies direct and inverted Codon Restrained Promoter Silencing method to the provided gene sequence',
       long_description=long_description,
@@ -11,12 +11,12 @@ setup(name='PSSM-promoter-tool',
       url='https://github.com/ellinium/PSSM_PromoterTool',
       author='Ellina Trofimova',
       author_email='ellina.trofimova@gmail.com',
-      license='GNU General Public Licensepython setup.py bdist_wheel',
+      license='GNU General Public License',
       install_requires=['biopython', 'click', 'cloudpickle', 'dask', 'distributed', 'fsspec', 'importlib-metadata','joblib',
                         'locket', 'msgpack', 'numpy', 'packaging', 'pandas', 'partd', 'psutil', 'python-dateutil', 'pytz',
                         'scikit-learn', 'scipy', 'six', 'sortedcontainers', 'tblib', 'threadpoolctl', 'toolz', 'tornado', 'urllib3',
                         'zict', 'zipp'],
-      py_modules=['PSSMPromoterCalculator'],
+      py_modules=['pssm_promoter_calculator'],
       zip_safe=False,
       keywords='promoter prediction transcription rate')
 
